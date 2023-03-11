5dd886a47ee3f78ec0a4644ad6cce5aceaea5358
Walter Liendo <walter.liendo@gmail.com>
2023-03-11 18:55:19 -0300

    incorporacion del input

--- js/app.js
+++ js/app.js
@@ -1,42 +1,43 @@
 let myDom = document; // document es un objeto global
 
 
-const myDog = {
-   name: "Firulais"
+// const myDog = {
+//    name: "Firulais"
 
-};
+// };
 
-const myCat = {
-   "name": "Kity"
+// const myCat = {
+//    "name": "Kity"
 
-};
+// };
 
 // ambas formas son validas
 
 
-console.log(myDog.name);
-console.log(myCat.name);
+// console.log(myDog.name);
+// console.log(myCat.name);
 
-class Mouse {
-    constructor(name, age ) {
-        this.name = name;
-        this.age = age;
+// class Mouse {
+//     constructor(name, age ) {
+//         this.name = name;
+//         this.age = age;
 
-   }
-}
+//    }
+// }
 
-const myMouse = new Mouse("Perez", 4);
+// const myMouse = new Mouse("Perez", 4);
 // let nombre = prompt("¿Cuál es tu nombre?");
 // alert(`¡Hola ${nombre}!`);
 
 /* Prompt */
-let name = prompt("Cual es tu nombre?")
+// let name = prompt("Cual es tu nombre?")
 
 /* div*/
-let myApp = document.getElementById("myApp");
+// let myApp = document.getElementById("myApp");
 
 /* reemplazo la info del html */
-myApp.innerHTML = "Hola " + name + "!, Bienvenido a mi sitio!!";
+// myApp.innerHTML = "Hola " + name + "!, Bienvenido a mi sitio!!";
+
 class Button {
     constructor (parentID, text){
         this.parentID = parentID;
@@ -44,14 +45,12 @@ class Button {
     }
     render() {
         let app = document.getElementById(this.parentID);
-        let button = document.createElement("button");
-        button.innerHTML = this.text;
-        app.appendChild(button);
+        app.innerHTML = `<button>${this.text}</button>`;
       }
     
 }
-  const btnLike = document.getElementById('app');
-      const likesCounter = document.getElementById('likes-counter');
+  let btnLike = document.getElementById('app');
+      let likesCounter = document.getElementById('likes-counter');
       
       let likes = 0;
       
@@ -60,7 +59,21 @@ class Button {
         likesCounter.textContent = likes;
       });
 // Crear instancia del botón con parentID y text
-const myButton = new Button("app", "Like");
+let myButton = new Button("app", "Botón de Likes");
 
 // Llamar el método render
-myButton.render()
+myButton.render();
+
+// Nuevo objeto imput
+class Input {
+  constructor(parentID, placeHolder){
+    this.parentID = parentID;
+    this.placeHolder = placeHolder;
+  };
+  render() {
+    let texto = document.getElementById(this.parentID);
+    texto.innerHTML = `<input type="text" placeholder="${this.placeHolder}">`;
+  }
+}
+let myInput = new Input("app2", "Ingrese texto aqui");
+myInput.render();
